3097e74a2de39aaf593ca5cb9eac9950ae4dea6a
Marc Julian Schwarz <67844154+marcjulianschwarz@users.noreply.github.com>
2020-12-19 19:27:19 +0100

    Create nasa-pictures.js

---
+++ nasa-pictures/nasa-pictures.js
@@ -0,0 +1,59 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: green; icon-glyph: magic;
+
+//#### Setup: ####
+// Get your own api key at: https://api.nasa.gov
+
+const api_key = ""
+let api_url = "https://api.nasa.gov/planetary/apod?api_key=" + api_key
+
+//##### End Setup ####
+
+async function getData(url){  
+  let request = new Request(url)  
+  let data = await request.loadJSON()
+  data.explanation = data.explanation.replace(/([.?!])\s*(?=[A-Z])/g, "$1|").split("|")[0]
+  return data
+}
+
+async function getImage(url){  
+  let img_request = new Request(url)  
+  let image = await img_request.loadImage()  
+  return image
+}
+
+async function createWidget(data, image){  
+  let widget = new ListWidget() 
+  widget.backgroundImage = image
+  widget.addSpacer()
+  
+  footer = widget.addStack()  
+  footer.layoutHorizontally()
+
+  desc_stack = footer.addStack()  
+  desc_stack.layoutVertically()
+  
+  title_txt = desc_stack.addText(data.title)
+  desc_stack.addSpacer(5)
+  explanation_txt = desc_stack.addText(data.explanation)
+  
+  footer.addSpacer()
+  
+  title_txt.textColor = Color.white()  
+  title_txt.font = Font.regularRoundedSystemFont(18)  
+  explanation_txt.textColor = Color.white()  
+  explanation_txt.font = Font.regularRoundedSystemFont(14)
+  
+  let widget_url = "http://www.google.com/search?q=" + data.explanation.replaceAll(" ", "+").replace("?", "")
+  widget.url = widget_url
+    
+  return widget
+}
+
+let data = await getData(api_url)
+let image = await getImage(data.hdurl)
+let widget = await createWidget(data, image)
+
+Script.setWidget(widget)
+Script.complete()
