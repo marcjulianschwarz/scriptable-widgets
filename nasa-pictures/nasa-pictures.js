a65bf994434a8045e2670dcee07a17f5e5dcb525
Marc Julian Schwarz <67844154+marcjulianschwarz@users.noreply.github.com>
2020-12-26 00:26:07 +0100

    Update nasa-pictures.js

--- nasa-pictures/nasa-pictures.js
+++ nasa-pictures/nasa-pictures.js
@@ -1,14 +1,13 @@
-// Variables used by Scriptable.
-// These must be at the very top of the file. Do not edit.
-// icon-color: green; icon-glyph: magic;
-
 //#### Setup: ####
-// Get your own api key at: https://api.nasa.gov
 
+// Get your own api key at: https://api.nasa.gov
 const api_key = ""
 let api_url = "https://api.nasa.gov/planetary/apod?api_key=" + api_key
 
+let size = config.widgetFamily
+
 //##### End Setup ####
+
 
 async function getData(url){  
   let request = new Request(url)  
@@ -17,33 +16,39 @@ async function getData(url){
   return data
 }
 
+
 async function getImage(url){  
   let img_request = new Request(url)  
   let image = await img_request.loadImage()  
   return image
 }
 
-async function createWidget(data, image){  
+
+async function createWidget(data, image){
   let widget = new ListWidget() 
   widget.backgroundImage = image
   widget.addSpacer()
   
   footer = widget.addStack()  
   footer.layoutHorizontally()
-
-  desc_stack = footer.addStack()  
+      
+  desc_stack = footer.addStack()
   desc_stack.layoutVertically()
   
   title_txt = desc_stack.addText(data.title)
-  desc_stack.addSpacer(5)
-  explanation_txt = desc_stack.addText(data.explanation)
+  
+  if(size == "large" || size == "medium"){
+    desc_stack.addSpacer(5)
+    explanation_txt = desc_stack.addText(data.explanation)  
+    explanation_txt.textColor = Color.white()  
+    explanation_txt.font = Font.regularRoundedSystemFont(14)
+  
+  }
   
   footer.addSpacer()
   
   title_txt.textColor = Color.white()  
   title_txt.font = Font.regularRoundedSystemFont(18)  
-  explanation_txt.textColor = Color.white()  
-  explanation_txt.font = Font.regularRoundedSystemFont(14)
   
   let widget_url = "http://www.google.com/search?q=" + data.explanation.replaceAll(" ", "+").replace("?", "")
   widget.url = widget_url
@@ -51,9 +56,14 @@ async function createWidget(data, image){
   return widget
 }
 
+
 let data = await getData(api_url)
 let image = await getImage(data.hdurl)
 let widget = await createWidget(data, image)
 
+widget.presentLarge()
+
 Script.setWidget(widget)
 Script.complete()
+
+
