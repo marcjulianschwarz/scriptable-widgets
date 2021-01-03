4a3061fec296c5cbd7ccce6a8f8f9f7cb6525210
Marc Julian Schwarz <67844154+marcjulianschwarz@users.noreply.github.com>
2021-01-03 23:12:01 +0100

    Update nasa-pictures.js
    Removed issues when widget would not be assigned. Quality of images down to lowest to save data volume and make widget updates faster.

--- nasa-pictures/nasa-pictures.js
+++ nasa-pictures/nasa-pictures.js
@@ -2,6 +2,8 @@
 // These must be at the very top of the file. Do not edit.
 // icon-color: red; icon-glyph: magic;
 
+
+//#### Start Setup ####
 // Get your own api key at: https://api.nasa.gov
 const api_key = ""
 let api_url = "https://api.nasa.gov/planetary/apod?api_key=" + api_key
@@ -10,11 +12,11 @@ let size = config.widgetFamily
 
 //##### End Setup ####
 
-
 async function getData(url){  
   let request = new Request(url)  
   let data = await request.loadJSON()
   data.explanation = data.explanation.replace(/([.?!])\s*(?=[A-Z])/g, "$1|").split("|")[0]
+  
   return data
 }
 
@@ -22,6 +24,7 @@ async function getData(url){
 async function getImage(url){        
   let img_request = new Request(url)    
   let image = await img_request.loadImage()
+  
   return image
 }
 
@@ -44,9 +47,8 @@ async function createWidget(data, image){
     explanation_txt = desc_stack.addText(data.explanation)  
     explanation_txt.textColor = Color.white()  
     explanation_txt.font = Font.regularRoundedSystemFont(14)  
-explanation_txt.shadowRadius = 1
-explanation_txt.shadowColor = Color.black()
-  
+    explanation_txt.shadowRadius = 1
+    explanation_txt.shadowColor = Color.black()
   }
   
   footer.addSpacer()
@@ -54,9 +56,8 @@ explanation_txt.shadowColor = Color.black()
   title_txt.textColor = Color.white()  
   title_txt.font = Font.boldRoundedSystemFont(18)  
 
-
-title_txt.shadowRadius = 1
-title_txt.shadowColor = Color.black()
+  title_txt.shadowRadius = 1
+  title_txt.shadowColor = Color.black()
   
   let widget_url = "http://www.google.com/search?q=" + data.explanation.replaceAll(" ", "+").replace("?", "")
   widget.url = widget_url
@@ -64,15 +65,15 @@ title_txt.shadowColor = Color.black()
   return widget
 }
 
+
 async function createErrorWidget(err){  
   let widget = new ListWidget()
   widget.addText("There was an error.")
   txt = widget.addText(err)
   txt.textColor = Color.red()
+  
   return widget
 }
-
-
 
 
 if(api_key == ""){
@@ -80,19 +81,16 @@ if(api_key == ""){
   let widget = await createErrorWidget("You must enter an API key at setup")
   widget.presentLarge()
   Script.setWidget(widget)
-  
 }else{
   try{
     let data = await getData(api_url)  
-    let image = await getImage(data.hdurl)  
-    let widget = await createWidget(data, image)  
+    let image = await getImage(data.url)  
+    let widget = await createWidget(data, image)
+    Script.setWidget(widget)
   }catch{
     console.error("There was a problem getting data from API")
     widget = await createErrorWidget("Problem with API, probably video which cant be used in a widget. New picture coming tomorrow.") 
-  }
-  widget.presentLarge()
-  
-  Script.setWidget(widget)
+    Script.setWidget(widget)
+  }  
 }
 Script.complete()
-
